a19d42a31af7f9c3dabfc4a6706be773b101bd0e
John Morris <john@zultron.com>
2013-08-24 22:11:02 -0500

    deployit.sh:  initial commit
    Used on dev side to copy files to webserver;
    
    Used on installer side to copy files from webserver

---
+++ deployit.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+#
+# Deploy the partman-auto-multiboot hack
+#
+
+DESTHOST=foo
+DESTDIR=/v/distro/provisioning/debian-multiboot-hack
+URL=http://web0.zultron.com/provisioning/debian-multiboot-hack
+
+if test -d /var/lib/anna-install; then
+    # in the debian installer miniroot
+
+    # ensure our directories exist
+    mkdir -p /lib/partman/lib
+
+    # fetch things
+    fetch-url $URL/autopartition-multiboot /bin/autopartition-multiboot
+    fetch-url $URL/initial_auto_multiboot \
+	/lib/partman/display.d/05initial_auto_multiboot
+    fetch-url $URL/auto-multiboot.sh /lib/partman/lib/auto-multiboot.sh
+
+    # make them executable
+    chmod +x \
+	/bin/autopartition-multiboot \
+	/lib/partman/display.d/05initial_auto_multiboot
+else
+    # in the dev environment
+    cd $(dirname $0)
+
+    # prepare a distribution
+    rm -rf deploy
+    mkdir -p deploy
+    cp autopartition-multiboot lib/auto-multiboot.sh \
+	display.d/initial_auto_multiboot $0 \
+	deploy
+
+    # deploy a tarball
+    ssh $DESTHOST mkdir -p $DESTDIR
+    tar czfC - deploy . | ssh $DESTHOST tar xzfC - $DESTDIR
+fi
